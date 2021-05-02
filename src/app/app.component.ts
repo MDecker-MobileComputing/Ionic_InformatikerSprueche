c9cb71d2f51ae4b42408b3f66c46068821d93d6e
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-02 23:47:01 +0200

    Browsersprache wird jetzt nach platform.ready() ausgewertet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { MenuController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,36 @@ import { MenuController } from '@ionic/angular';
 })
 export class AppComponent {
 
-  /**
-   * Default-Sprache auf Englisch setzen.
-   */
   constructor(private translate: TranslateService, 
-              private menuController: MenuController) {
+              private menuController: MenuController,
+              private platform: Platform) {
 
-    this.translate.setDefaultLang("en");
-    //this.translate.setDefaultLang("de");
+      this.platform.ready().then( () => { this.uebersetzungInitialisieren(); } );
+  }
+
+  /**
+   * Übersetzung initialisieren, d.h. u.a. die aktuelle Browsersprache auswerten.
+   * Lösung nach https://masteringionic.com/blog/2018-07-14-creating-a-multi-language-ionic-translation-app-with-ngx-translate/
+   * 
+   * Diese Methode darf erst dann aufgerufen werden, wenn der von der Methode Platform::read() zurückgegebene Intent
+   * aufgelöst wurde.
+   */
+  uebersetzungInitialisieren() {
+
+        // Wenn ein Text in aktueller Sprache nicht vorhanden ist, dann englischen Text anzeigen
+        this.translate.setDefaultLang("en");
+
+        if (this.translate.getBrowserLang() !== undefined) {
+        
+            let browserSprache = this.translate.getBrowserLang();
+            console.log(`browsersprache=${browserSprache}`);
+            this.translate.use(browserSprache);
+    
+        } else  {
+    
+            this.translate.use("en"); // Fallback-Sprache
+            console.log("Fallback-Sprache wird gesetzt!");
+        }    
   }
 
   /**
