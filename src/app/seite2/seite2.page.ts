3e06a3516367f4fe0cd9de9af68bf85dc0c57d5e
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-02 22:32:22 +0200

    seite2: Nummern-Array wird jetzt programmatisch gefuellt.

--- src/app/seite2/seite2.page.ts
+++ src/app/seite2/seite2.page.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Seite2Page {
 
+  /** Anzahl der Sprüche, die in den i18n-Dateien definiert ist. */
+  private readonly ANZ_SPRUECHE = 4;
+
+  /** 
+   * Array mit Zahlen von 1 bis ANZ_SPRUECHE füllen. 
+   * Lösung nach https://basarat.gitbook.io/typescript/main-1/create-arrays
+   */
+  private nummernArray = [...new Array(this.ANZ_SPRUECHE)].map((_,i) => i+1 );
+
   constructor() { }  
 
 }
